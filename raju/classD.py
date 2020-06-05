7bcc8dbe0a990345016c65010cec0b7598932e8c
rajuf6 <rajupanugantif6@gmail.com>
2020-06-05 16:05:57 +0530

    first commit

---
+++ raju/classD.py
@@ -0,0 +1,11 @@
+class student:
+    def __init__(self,name,age,rank):
+        self.name=name
+        self.age=age
+        self.rank=rank
+
+s1=student('raju','25','2')
+s2=student('vikram','3','26')
+
+print(s1.name,s1.age,s1.rank)
+print(s2.name,s2.age,s2.rank)
