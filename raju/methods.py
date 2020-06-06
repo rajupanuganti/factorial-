ffab961a8897005986792f4c04d45985335333d9
rajuf6 <rajupanugantif6@gmail.com>
2020-06-06 22:15:13 +0530

    first commit

--- raju/methods.py
+++ raju/methods.py
@@ -5,10 +5,10 @@ class student:
         self.m2=m2
         self.m3=m3
 
-    def avg(self):
-        return (self.m1+self.m2+self.m3)/3
+    def info(cls):
+        return cls.school
+
 
 s1=student(20,23,25)
 s2=student(48,45,23)
-print(s1.avg())
-print(s2.avg())
+print(s1,avg())
